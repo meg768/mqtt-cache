cf4fd7b6a5f606da2b418b90e3c6a9e3c94d4bcf
meg768 <meg768@users.noreply.github.com>
2023-04-23 22:09:34 +0200

    -

---
+++ mqtt-cache.js
@@ -0,0 +1,19 @@
+module.exports = function MqttCache(client) {
+
+    let publish = client.publish;
+    let cache = {};
+
+    client.publish = (topic, message, ...args) => {
+
+        if (cache[topic] == undefined || cache[topic] != message) {
+            cache[topic] = message;
+            console.log(`Publishing ${topic}:${message}`);
+            return publish.call(client, topic, message, ...args);
+
+        }
+
+    }
+
+
+    return client;
+}
